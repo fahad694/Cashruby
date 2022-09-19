06712430e70c0faa3833e0a97d1700ffdc0d473c
fahad694 <tariqfahad650@gmail.com>
2022-09-19 19:04:06 +0500

    initial changes categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,32 @@
 class CategoriesController < ApplicationController
   def new
   	@category = Category.new
- 
   end
   def create
-  	@create = Category.create
 
+  	@category = Category.create(category_params)
+    redirect_to categories_path
+  end
+  def index
+    @category = Category.all
+    
+  end
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+  def edit
+    @category = Category.find(params[:id])
+    render :edit
+  end
+  def update
+    @category = Category.find(params[:id])
+    @category = @category.update(category_params)
+    redirect_to categories_path
   end
 end
+
+
