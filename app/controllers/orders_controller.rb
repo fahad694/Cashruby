81660a0791ebe6d6fc78e9d8be660dbc0ce3900a
fahad694 <tariqfahad650@gmail.com>
2022-09-27 17:11:51 +0500

    Order management

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,24 @@
 class OrdersController < ApplicationController
   before_action :load_id, only: [:commit_order, :show, :remove_item, :payment]
+  # GET order new
   def new
     @order = Order.new
   end
 
+  # POST create user
   def create
     @user = User.first
     @order = Order.create(customer_id: @user.id)
     redirect_to order_path(@order)
   end
   
+  # GET user
   def show
     @items = Item.all
     @order_items = OrderItem.all
   end
 
+  # Custome route POST create OrderItem
   def commit_order
     item_id = params["items"]["0"]
     order_id = params["id"]
@@ -29,12 +33,15 @@ class OrdersController < ApplicationController
     @order.recalculate_total_price
     redirect_to order_path(@order)
   end
+
+  # Delete OrdetItem
   def remove_item
     @order_item = OrderItem.find(params[:order_item_id])
     @order_item.destroy
     redirect_to order_path(params[:id])
   end
 
+  # POST create payment
   def payment
     
     @payment = Payment.create(order_id: params[:id], paid_amount: params[:paid_amount])
@@ -43,8 +50,10 @@ class OrdersController < ApplicationController
     redirect_to order_path(params[:id])
   end
   private
-def load_id
-  @order = Order.find(params[:id])
-end
+
+  # GET find Order
+  def load_id
+    @order = Order.find(params[:id])
+  end
 
 end
--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,43 +1,53 @@
 class SubCategoriesController < ApplicationController
-
+  before_action :load_Subcategory, only: [:show, :edit, :update, :destroy] 
+  # GET subcategory
   def new
     @categories = Category.all
     @subcategory = SubCategory.new
   end
 
+  # POST create subcategory
   def create
     @subcategory = SubCategory.create(sub_category_params)
     redirect_to sub_categories_path()  
   end
   
+  # GET show all subcategory
   def index
     @subcategories = SubCategory.all
   end
 
+  # GET show subcategory 
   def show
-    @subcategory = SubCategory.find(params[:id])
-
   end
 
+  #  GET subcategory
   def edit
-    @subcategory = SubCategory.find(params[:id])
     @categories = Category.all
   end
 
+  # PATCH/PUT subcategory
   def update
-  @categories = Category.all
-  @subcategory = SubCategory.find(params[:id])
-  @subcategory.update(sub_category_params)
+    @categories = Category.all
+    @subcategory.update(sub_category_params)
     redirect_to sub_categories_path()
   end
+
+  # DELETE subcategory
   def destroy
-    @subcategory = SubCategory.find(params[:id])
     @subcategory.destroy
     redirect_to sub_categories_path
   end
   private
 
+  # load subcategory
+  def load_Subcategory
+    @subcategory = SubCategory.find(params[:id])
+  end
+
+  # strong parameter for create
   def sub_category_params
     params.require(:sub_category).permit(:title,:category_id)
   end
+  
 end
