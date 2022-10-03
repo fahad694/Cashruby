04628dc3e0bc41f9634ebdcf7189a73ef05d67c7
fahad694 <tariqfahad650@gmail.com>
2022-10-03 12:39:27 +0500

    helper and views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def sub_category_options(sub_categories)
+  options_for_select(@sub_categories.map do |sub_category|
+  [sub_category.title, sub_category.id]  end )
+  end
 end
